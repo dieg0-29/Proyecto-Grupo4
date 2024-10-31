3a34e010502a9921416f91c22beefc0d546b29d5
Diego <diego.pinedo1229@gmail.com>
2024-10-31 09:27:27 -0500

    Prueba2

---
+++ ProyectoTransporte (1)/ProyectoTransporte/src/main/java/pe/edu/uni/ProyectoTransporte/ProyectoTransporteApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.uni.ProyectoTransporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTransporteApplication.class, args);
+	}
+
+}
---
+++ ProyectoTransporte (1)/ProyectoTransporte/src/test/java/pe/edu/uni/ProyectoTransporte/ProyectoTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.uni.ProyectoTransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
