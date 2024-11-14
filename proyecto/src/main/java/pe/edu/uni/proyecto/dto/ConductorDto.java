88e2bee7718df13225a820e1aa9a7ebd2943e36d
HarryPC2023 <harrypc2021@hotmail.com>
2024-11-13 21:24:32 -0500

    khniuhbiu

--- proyecto/src/main/java/pe/edu/uni/proyecto/dto/ConductorDto.java
+++ proyecto/src/main/java/pe/edu/uni/proyecto/dto/ConductorDto.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 
 @Data @AllArgsConstructor @NoArgsConstructor
 public class ConductorDto {
-<<<<<<< HEAD
+
 	private int id_cond;
 	private String nombre;
 	private String apellido;
@@ -14,12 +14,5 @@ public class ConductorDto {
 	private String correo;
 	private int telefono;
 	private double salario;
-=======
-	private String idConductor;
-	private String nombre;
-	private String apellido;
-	private String dni;
-	private String correo;
-	private String salario;
->>>>>>> e3f2c5c858d4bdb1387a30a35797261816f43e27
+
 }
