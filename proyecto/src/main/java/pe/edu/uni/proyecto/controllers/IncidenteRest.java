e64b32c92b4f0f314c8228502ffbbeb2bb17fa3f
Joel <joel.yaures.c@uni.pe>
2024-11-27 17:08:57 -0500

    as

--- proyecto/src/main/java/pe/edu/uni/proyecto/controllers/IncidenteRest.java
+++ proyecto/src/main/java/pe/edu/uni/proyecto/controllers/IncidenteRest.java
@@ -1,34 +1,39 @@
 package pe.edu.uni.proyecto.controllers;
 
+import java.time.format.DateTimeParseException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import pe.edu.uni.proyecto.dto.IncidenteDto;
+import pe.edu.uni.proyecto.dto.MantenimientoDto;
+import pe.edu.uni.proyecto.dto.ResponseMessage;
 import pe.edu.uni.proyecto.service.IncidenteService;
 
 @RestController
+@CrossOrigin("http://localhost:5500")
 @RequestMapping("/api/registrar")
 public class IncidenteRest {
 	@Autowired
 	private IncidenteService incidenteService;
 
 	@PostMapping("/incidente")
-	public ResponseEntity<String> incidente(@RequestBody IncidenteDto bean) {
-		try {
-			incidenteService.reportarincidente(bean);
-			return new ResponseEntity<>("Incidente registrado exitosamente.", HttpStatus.CREATED);
-
-		} catch (IllegalArgumentException e) {
-			return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
-		} catch (RuntimeException e) {
-			return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
-		} catch (Exception e) {
-			return new ResponseEntity<>("Error inesperado: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+	 public ResponseEntity<ResponseMessage> registrarReparacion(@RequestBody IncidenteDto bean) throws DateTimeParseException {
+        try {
+            incidenteService.reportarincidente(bean);
+            return new ResponseEntity<>(new ResponseMessage("Incidente registrado exitosamente."), HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(new ResponseMessage("Error: " + e.getMessage()), HttpStatus.BAD_REQUEST);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(new ResponseMessage("Error: " + e.getMessage()), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ResponseMessage("Error inesperado: " + e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 	}
 }
--- proyecto/src/main/java/pe/edu/uni/proyecto/dto/IncidenteDto.java
+++ proyecto/src/main/java/pe/edu/uni/proyecto/dto/IncidenteDto.java
@@ -1,5 +1,7 @@
 package pe.edu.uni.proyecto.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +9,15 @@ import lombok.NoArgsConstructor;
 @Data @AllArgsConstructor @NoArgsConstructor
 public class IncidenteDto {
 	
+	@JsonProperty("id_empleado")
 	private int empleado;
+	@JsonProperty("programacion")
 	private int programacion;
+	@JsonProperty("tipo_incidente")
 	private int tipo_incidente;
+	@JsonProperty("fecha")
 	private String fecha;
+	@JsonProperty("detalle")
 	private String detalle;
 	
 }
