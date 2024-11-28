ebe13ea56debfcb3acfb250ae0ec0460b1c7ca13
Joel <joel.yaures.c@uni.pe>
2024-11-28 08:05:13 -0500

    asd

--- proyecto/src/main/java/pe/edu/uni/proyecto/controllers/ReparacionRest.java
+++ proyecto/src/main/java/pe/edu/uni/proyecto/controllers/ReparacionRest.java
@@ -1,17 +1,23 @@
 package pe.edu.uni.proyecto.controllers;
 
+import java.time.format.DateTimeParseException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import pe.edu.uni.proyecto.dto.IncidenteDto;
 import pe.edu.uni.proyecto.dto.ReparacionDto;
+import pe.edu.uni.proyecto.dto.ResponseMessage;
 import pe.edu.uni.proyecto.service.ReparacionService;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:5500", "http://127.0.0.1:5500"})
 @RequestMapping("/api/registrar/reparacion")
 public class ReparacionRest {
 
@@ -19,16 +25,16 @@ public class ReparacionRest {
     private ReparacionService reparacionService;
 
     @PostMapping
-    public ResponseEntity<String> registrarReparacion(@RequestBody ReparacionDto bean) {
+    public ResponseEntity<ResponseMessage> registrarReparacion(@RequestBody ReparacionDto bean) throws DateTimeParseException {
         try {
             reparacionService.reparacion(bean);
-            return new ResponseEntity<>("Reparación registrada exitosamente.", HttpStatus.CREATED);
+            return new ResponseEntity<>(new ResponseMessage("Incidente registrado exitosamente."), HttpStatus.CREATED);
         } catch (IllegalArgumentException e) {
-            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ResponseMessage("Error: " + e.getMessage()), HttpStatus.BAD_REQUEST);
         } catch (RuntimeException e) {
-            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new ResponseMessage("Error: " + e.getMessage()), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>("Error inesperado: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ResponseMessage("Error inesperado: " + e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
+	}
 }
--- proyecto/src/main/java/pe/edu/uni/proyecto/dto/ReparacionDto.java
+++ proyecto/src/main/java/pe/edu/uni/proyecto/dto/ReparacionDto.java
@@ -1,5 +1,7 @@
 package pe.edu.uni.proyecto.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,12 +10,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReparacionDto {
+	
+	@JsonProperty("idempleado")
 	private int idempleado;
+	 
+	@JsonProperty("idincidente")
 	private int idincidente;
+	
+	@JsonProperty("idtaller")
 	private int idtaller;
+	
+	@JsonProperty("fechareparacion")
 	private String fechareparacion;
+	
+	@JsonProperty("calificacion")
 	private double calificacion;
+	
+	@JsonProperty("costo")
 	private double costo;
+	
+	@JsonProperty("detalle")
 	private String detalle;
 
 }
