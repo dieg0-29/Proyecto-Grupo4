41a1aee43feb3d4ac90134ff66b1230d251a4524
Joel <joel.yaures.c@uni.pe>
2024-12-01 17:50:01 -0500

    as

--- frontend_proyecto/login/login.js
+++ frontend_proyecto/login/login.js
@@ -1,5 +1,6 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('loginForm').addEventListener('submit', function(event) {   
     event.preventDefault(); // Evita que el formulario se envíe
+    
     const username = document.getElementById('username').value; // Obtener el nombre de usuario
     localStorage.setItem('username', username); // Guardar el nombre de usuario en localStorage
     window.location.href = 'http://127.0.0.1:5500/frontend_proyecto/paginaprincipal/index.html'; 
